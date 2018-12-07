6b78f2ab46a915509e8ed8af83b82259f4bdcb32
Alexander Alekhin <alexander.alekhin@intel.com>
2018-12-07 17:08:10 +0300

    master: linux64 builders uses Ubuntu 16.04 image

--- cmake_checks/precommit_armv8/POST_FINALIZE.cmake
+++ cmake_checks/precommit_armv8/POST_FINALIZE.cmake
@@ -1,15 +1,3 @@
-build_validate_compiler_flags(CMAKE_CXX_FLAGS
-"-fdata-sections -Wa,--noexecstack -fsigned-char -Wno-psabi -fsigned-char -W -Wall -Werror=return-type -Werror=non-virtual-dtor -Werror=address -Werror=sequence-point -Wformat -Werror=format-security -Wmissing-declarations -Wundef -Winit-self -Wpointer-arith -Wshadow -Wsign-promo -Wuninitialized -Winit-self -Wno-narrowing -Wno-delete-non-virtual-dtor -Wno-comment -fdiagnostics-show-option -pthread -fomit-frame-pointer -ffunction-sections -fdata-sections -fvisibility=hidden -fvisibility-inlines-hidden"
-)
-
-build_validate_compiler_flags(CMAKE_CXX_FLAGS_RELEASE
-"-O3 -DNDEBUG -DNDEBUG"
-)
-
-build_validate_compiler_flags(CMAKE_CXX_FLAGS_DEBUG
-"-g -O0 -DDEBUG -D_DEBUG"
-)
-
 build_validate_disabled_or_undefined_flag(BUILD_opencv_python2)
 build_validate_disabled_or_undefined_flag(BUILD_opencv_python3)
 
--- cmake_checks/precommit_linux64/POST_COMPILER_OPTIONS.cmake
+++ cmake_checks/precommit_linux64/POST_COMPILER_OPTIONS.cmake
@@ -3,7 +3,7 @@ build_validate(CMAKE_SYSTEM_PROCESSOR "x86_64")
 build_validate(CMAKE_CXX_COMPILER_ID "GNU")
 
 build_validate_compiler_flags(CMAKE_CXX_FLAGS
-"-fsigned-char -W -Wall -Werror=return-type -Werror=non-virtual-dtor -Werror=address -Werror=sequence-point -Wformat -Werror=format-security -Wmissing-declarations -Wundef -Winit-self -Wpointer-arith -Wshadow -Wsign-promo -Wuninitialized -Winit-self -Wno-narrowing -Wno-delete-non-virtual-dtor -Wno-comment -Wno-missing-field-initializers -fdiagnostics-show-option -Wno-long-long -pthread -fomit-frame-pointer -ffunction-sections -fdata-sections -msse -msse2 -msse3 -fvisibility=hidden -fvisibility-inlines-hidden"
+"-fsigned-char -W -Wall -Werror=return-type -Werror=non-virtual-dtor -Werror=address -Werror=sequence-point -Wformat -Werror=format-security -Wmissing-declarations -Wundef -Winit-self -Wpointer-arith -Wshadow -Wsign-promo -Wuninitialized -Winit-self -Wsuggest-override -Wno-narrowing -Wno-delete-non-virtual-dtor -Wno-comment -fdiagnostics-show-option -Wno-long-long -pthread -fomit-frame-pointer -ffunction-sections -fdata-sections -msse -msse2 -msse3 -fvisibility=hidden -fvisibility-inlines-hidden"
 )
 
 build_validate_compiler_flags(CMAKE_CXX_FLAGS_RELEASE
@@ -15,4 +15,4 @@ build_validate_compiler_flags(CMAKE_CXX_FLAGS_DEBUG
 )
 
 build_validate_list(CPU_BASELINE_FINAL "SSE;SSE2;SSE3")
-build_validate_list(CPU_DISPATCH_FINAL "SSE4_1;SSE4_2;FP16;AVX;AVX2")
+build_validate_list(CPU_DISPATCH_FINAL "SSE4_1;SSE4_2;FP16;AVX;AVX2;AVX512_SKX")
