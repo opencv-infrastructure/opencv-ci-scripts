5258c2f30bc74751d82274538eabe7f003130d4a
Alexander Alekhin <alexander.a.alekhin@gmail.com>
2020-12-05 21:06:27 +0000

    CMAKE_CXX_FLAGS: removed duplicate -Winit-self

--- cmake_checks/precommit_armv7/POST_COMPILER_OPTIONS.cmake
+++ cmake_checks/precommit_armv7/POST_COMPILER_OPTIONS.cmake
@@ -3,7 +3,7 @@ build_validate(CMAKE_SYSTEM_PROCESSOR "arm")
 build_validate(CMAKE_CXX_COMPILER_ID "GNU")
 
 build_validate_compiler_flags(CMAKE_CXX_FLAGS
-"-mthumb -fdata-sections -Wa,--noexecstack -fsigned-char -Wno-psabi -fsigned-char -W -Wall -Werror=return-type -Werror=non-virtual-dtor -Werror=address -Werror=sequence-point -Wformat -Werror=format-security -Wmissing-declarations -Wundef -Winit-self -Wpointer-arith -Wshadow -Wsign-promo -Wuninitialized -Winit-self -Wno-delete-non-virtual-dtor -Wno-comment -Wno-missing-field-initializers -fdiagnostics-show-option -pthread -fomit-frame-pointer -ffunction-sections -fdata-sections -mfpu=neon-fp16 -mfp16-format=ieee -fvisibility=hidden -fvisibility-inlines-hidden"
+"-mthumb -fdata-sections -Wa,--noexecstack -fsigned-char -Wno-psabi -fsigned-char -W -Wall -Werror=return-type -Werror=non-virtual-dtor -Werror=address -Werror=sequence-point -Wformat -Werror=format-security -Wmissing-declarations -Wundef -Winit-self -Wpointer-arith -Wshadow -Wsign-promo -Wuninitialized -Wno-delete-non-virtual-dtor -Wno-comment -Wno-missing-field-initializers -fdiagnostics-show-option -pthread -fomit-frame-pointer -ffunction-sections -fdata-sections -mfpu=neon-fp16 -mfp16-format=ieee -fvisibility=hidden -fvisibility-inlines-hidden"
 )
 
 build_validate_compiler_flags(CMAKE_CXX_FLAGS_RELEASE
